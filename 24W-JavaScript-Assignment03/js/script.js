abf036e45d1e9d61581bd420c841663616b9e99f
Great-BC <122874308+Great-BC@users.noreply.github.com>
2024-03-24 20:57:15 -0400

    Assignmnent 3

---
+++ 24W-JavaScript-Assignment03/js/script.js
@@ -0,0 +1,44 @@
+
+// Capture form values, create Pizza object and add student information
+document.getElementById('order-btn').addEventListener('click', function () {
+    var studentInfo = document.createElement('p');
+    studentInfo.textContent = "Student ID: 200555844";
+    document.getElementById('student-info').appendChild(studentInfo);
+    var size = document.querySelector('input[name="size"]:checked').value;
+    var crust = document.querySelector('input[name="crust"]:checked').value;
+    var sauce = document.querySelector('input[name="sauce"]:checked').value;
+    var cheese = [];
+    document.querySelectorAll('input[name="cheese"]:checked').forEach(function (checkbox) {
+        cheese.push(checkbox.value);
+    });
+    var toppings = [];
+    document.querySelectorAll('input[name="toppings"]:checked').forEach(function (checkbox) {
+        toppings.push(checkbox.value);
+    });
+
+    // Pizza object
+    var pizza = new Pizza(size, crust, sauce, cheese, toppings);
+
+    // Output pizza description
+    var pizzaDescription = document.getElementById('pizza-description');
+    pizzaDescription.innerHTML = `
+        <h2>Your Pizza</h2>
+        <p>Size: ${pizza.size}</p>
+        <p>Crust: ${pizza.crust}</p>
+        <p>Sauce: ${pizza.sauce}</p>
+        <p>Cheese: ${pizza.cheese.join(', ')}</p>
+        <p>Toppings: ${pizza.toppings.join(', ')}</p>
+    `;
+});
+
+// Pizza object class
+class Pizza {
+    constructor(size, crust, sauce, cheese, toppings) {
+        this.size = size;
+        this.crust = crust;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.toppings = toppings;
+    }
+}
+
